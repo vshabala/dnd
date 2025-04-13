9acbb0bee02e6f835f24fab9e193a76edda2de5a
Valentyna Shabala <vshabala@gmail.com>
2025-04-13 07:38:11 +0300

    a mistake in the description text was corrected

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <h1>Tables with drag-n-drop rows</h1>
       <p>
         You can drag lines from the right table and drop them into the left
-        table. The item ids will be collected in the right table.
+        table. The item ids will be collected in the left table.
       </p>
       <DragDropTables />
     </>
